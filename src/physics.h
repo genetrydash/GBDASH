48b21e7a45a3b3887f90d3b71e0b63ae5ce829b2
AArt1256 <aart1256@outlook.com>
2025-05-27 18:26:49 +0300

    remove mul and div usage from physics.c and use bit-shifts instead (MUCH FASTER!)

--- src/physics.h
+++ src/physics.h
@@ -1,17 +1,21 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
+// PLEASE USE 8-BIT OR 16-BIT INTEGERS
+// DO NOT USE INT (IT IS 32-BIT IN GBDK WHICH MEANS MORE SLOWDOWN!!!!)
+#include <stdint.h>
 
-#define GRAVITY 16
-#define JUMP_STRENGTH 192
+
+#define GRAVITY 14
+#define JUMP_STRENGTH 180
 
 #define MOVE_SPEED_X 88 // Horizontal movement speed
-#define MODIFIER 64
+#define MODIFIER_SHIFT 6
 
 #define GROUND_Y 128  // Ground level (Y position on screen)
-#define MAX_SPEED (7 * MODIFIER)
+#define MAX_SPEED (7 << MODIFIER_SHIFT)
 
-extern int x_pos;
-extern int y_pos;
+extern uint32_t x_pos;
+extern int16_t y_pos;
 extern unsigned int player_y;
 extern unsigned int player_x;
 
